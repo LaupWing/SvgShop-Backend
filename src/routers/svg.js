9ede38b5d030c48ae464b948a043e9c61e9afcae
LaupWing <loc_strict@hotmail.com>
2019-11-09 17:31:55 +0100

    adding svg works

--- src/routers/svg.js
+++ src/routers/svg.js
@@ -1,13 +1,20 @@
 const express = require('express')
 const router = new express.Router()
-const Svg = require('../models/svg')
+const SVG = require('../models/svg')
 
 router
     .get('/user/svg', (req,res)=>{
         res.send('users svgs')
     })
-    .post('/user/svg', (req,res)=>{
-        res.send('posting svg')
+    .post('/user/svg', async (req,res)=>{
+        const svg = new SVG(req.body)
+        try{
+            await svg.save()
+            res.status(201).send('Succes')
+        }
+        catch(e){
+            res.status(400).send('Something went wrong in the server')
+        }
     })
     .get('/svg', (req,res)=>{
         res.send('show all svg')
