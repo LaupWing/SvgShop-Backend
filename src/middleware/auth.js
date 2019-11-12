e8a96203bd72d3b6cd2fe51237aa60ab61f0455b
LaupWing <loc_strict@hotmail.com>
2019-11-12 14:58:52 +0100

    added proper auth

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -1,5 +1,19 @@
+const jwt = require('jsonwebtoken')
+const User = require('../models/user')
+
 const auth = async (req,res,next)=>{
-    console.log('Auth Middleware')
+    try{
+        const token = req.header('Authorization').replace('Bearer ', '')
+        const decoded = jwt.verify(token, process.env.JWT_SECRET)
+
+        const user = await User.findOne({_id: decoded._id, 'tokens.token': token})
+        if(!user){
+            throw new Error()
+        }
+    }
+    catch(e){
+        res.status(400).send({error: 'Please Authenticate properly by using the right the force'})
+    }
     next()
 }
 
